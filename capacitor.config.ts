e2f77a503dc5a7232c67b777e27a84d61d7e96be
Samuel Purriños <samuel.purrinos@outlook.com>
2023-03-12 21:36:57 +0100

    Minor fixes for jenkins android build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'elevator-measurement-calculator',
+  appId: 'com.uichesoh.agrelo-elevators',
+  appName: 'algrelo-elevators',
   webDir: 'www',
   bundledWebRuntime: false
 };
