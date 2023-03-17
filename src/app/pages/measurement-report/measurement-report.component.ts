b303935e596cced92525f0e251efe9740205f094
Samuel Purriños <samuel.purrinos@outlook.com>
2023-03-17 15:36:23 +0100

    Added PDF report generation and migrated project from Capacitor to Cordova

--- src/app/pages/measurement-report/measurement-report.component.ts
+++ src/app/pages/measurement-report/measurement-report.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Measurement } from 'src/app/models/measurement';
 import { StorageService } from '../../services/storage.service';
 import { StoredMeasurement } from '../../models/stored.measurement.interface';
-
+declare var cordova:any; 
 @Component({
   selector: 'app-measurement-report',
   templateUrl: './measurement-report.component.html',
@@ -21,9 +21,30 @@ export class MeasurementReportComponent implements OnInit {
   }
 
   generateReport() {
-    console.log(this.storedMeasurement);
+    cordova.plugins.pdf.htmlToPDF({
+      data: document.getElementById('report')!.innerHTML,
+      documentSize: "A4",
+      landscape: "portrait",
+      type: "share",
+      fileName: "report.pdf"
+    }
+    ).then((
+      //This method saves the pdf to the device
+      (data:any) => {
+      cordova.plugins.file.writeFile(cordova.file.externalRootDirectory, 'report.pdf', data, {replace:true}, (success:any) => {
+        console.log('success');
+      }
+      ).catch((error:any) => {
+        console.log(error);
+      }
+      );
+
+    })  
+    ).catch((error:any) => {
+      console.log(error);
+    }
+    );
   }
-  
 
   ngOnInit() {}
 
